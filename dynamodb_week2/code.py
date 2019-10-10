e30d8a92fdd497cd00ff5e2fe1a322081b6edf69
b-jayesh9 <jayshu.in@gmail.com>
2019-10-10 13:18:06 +0530

    Add files via upload

--- dynamodb_week2/code.py
+++ dynamodb_week2/code.py
@@ -2,37 +2,46 @@ import boto3
 
 dynamodb = boto3.resource('dynamodb')
 
-table_name = 'mynewtable'
 existing_tables = [table.name for table in dynamodb.tables.all()]
 
-#sample list to insert
 
-accounts_list = [['abc@ab.co','asdas','eirj'], ['q4esf@fds.co','fsdsfsd','a3r3f'], ['h4w4@ab.co','gw4g4','hwhw'],
+# sample list to insert
+
+accounts_list = [['abc@ab.co', 'asdas', 'eirj'], ['q4esf@fds.co', 'fsdsfsd', 'a3r3f'], ['h4w4@ab.co', 'gw4g4', 'hwhw'],
                  ['whww@ab.co', 'wwgs', 'h4wh4']]
 
-#we can add a file/other source to add data to the table.
+# we can add a file/other source to add data to the table.
 
 
-def create_table():
+def create_table(tbl, attr1, attr2):
+
+    '''
+
+    :param tbl:  name of the table to be created
+    :param attr1: name of 1st attribute in key schema
+    :param attr2: name of 2nd attribute in key schema
+    :return: returns if table is successfully created or not by using inbuilt function table.table_status
+    '''
+
     table = dynamodb.create_table(
-        TableName=table_name,
+        TableName=tbl,
         KeySchema=[
             {
-                'AttributeName': 'username',
+                'AttributeName': attr1,
                 'KeyType': 'HASH'
             },
             {
-                'AttributeName': 'password',
+                'AttributeName': attr2,
                 'KeyType': 'RANGE'
             }
         ],
         AttributeDefinitions=[
             {
-                'AttributeName': 'username',
+                'AttributeName': attr1,
                 'AttributeType': 'S'
             },
             {
-                'AttributeName': 'password',
+                'AttributeName': attr2,
                 'AttributeType': 'S'
             },
 
@@ -43,38 +52,44 @@ def create_table():
              'WriteCapacityUnits': 50
          }
     )
-    print(table.item_count)
+
     return table.table_status
 
 
-def put_data():
-    table = dynamodb.Table(table_name)
+def put_data(tbl, user, pwd, name):
+    table = dynamodb.Table(tbl)
     response = table.put_item(
         Item={
-            'username': 'eranahaata@gmail.com',
-            'password': 'qazxsw',
-            'name': 'Era Nsaha'
+            'username': user,
+            'password': pwd,
+            'name': name
         }
-
     )
-    return response
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        return True
+    else:
+        return False
 
 
-def get_data():
-    table = dynamodb.Table(table_name)
+def get_data(tbl, user, pwd):
+    table = dynamodb.Table(tbl)
     response = table.get_item(
         AttributesToGet=[
             'username',
             'password'],
-        Key={'username': "eranahaata@gmail.com", 'password': "qazxsw"}
+        Key={'username': user, 'password': pwd}
 
     )
-    print(response)
-    return response
+    if 'Item' in response:
+        print(response['Item'])
+        return response['Item']
+    else:
+        return None
 
 
-def write_batch():
-    with dynamodb.Table(table_name).batch_writer() as batch:
+def write_batch(tbl):
+    with dynamodb.Table(tbl).batch_writer() as batch:
+        print("assad")
         for i in range(len(accounts_list)):
             batch.put_item(
                 Item={
@@ -83,5 +98,56 @@ def write_batch():
                     'name': accounts_list[i][2]
                 }
             )
+    return dynamodb.Table(tbl).item_count
 
+
+def delete_table(tbl):
+    if tbl in existing_tables:
+        table = dynamodb.Table(tbl)
+        table.delete()
+        dynamodb.Table(tbl).wait_until_not_exists()
+        return "table deleted successfully" if tbl not in existing_tables else "Error in deleting table"
+    else:
+        return "No such table exists"
+
+
+def update_item(tbl, user, pwd, key_to_insert, value_to_insert):
+    table = dynamodb.Table(tbl)
+    table.update_item(
+        Key={
+            'username': user,
+            'password': pwd
+        },
+        UpdateExpression='SET {} = :val1'.format(key_to_insert),
+        ExpressionAttributeValues={
+            ':val1': value_to_insert
+        }
+    )
+
+    return 0
+
+
+def delete_item(tbl, key_val_pair):
+    response = tbl.delete_item(
+        Key={key_val_pair['key']: key_val_pair['value']}
+    )
+    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        return True
+    else:
+        return False
+    return 0
+
+
+def delete_all_items(tbl_to_delete_items):
+    if tbl_to_delete_items in existing_tables:
+        table = dynamodb.Table(tbl_to_delete_items)
+        table.delete()
+        try:
+            dynamodb.Table(tbl_to_delete_items).wait_until_not_exists()
+            table = create_table(tbl_to_delete_items)
+        except Exception:
+            print("Error occurred while deleting items in {} table".format(tbl_to_delete_items))
+    else:
+        print("No such table exists")
+    return 0
 
--- dynamodb_week2/helper.py
+++ dynamodb_week2/helper.py
@@ -1,13 +1,38 @@
 import code as c
-
+table_name = 'my_new_table'
 if __name__ == '__main__':
-    if c.table_name not in c.existing_tables:
-        c.create_table()
-        c.write_batch()
-        c.get_data()
-    elif c.table_name in c.existing_tables:
-        try:
-            c.write_batch()
-            c.get_data()
-        except Exception:
-            c.get_data()
+    if table_name not in c.existing_tables:
+        c.create_table('my_new_table', 'username', 'password')
+        # takes 3 parameters: table_name, key attribute 1 and key attribute 2
+
+        c.put_data('my_new_table', 'abc@xyz.com', 'qazxsw', 'jay')
+        # takes 4 parameters: table_name, key attribute 1, key attribute 2 and some other attribute
+
+        c.get_data('my_new_table', 'abc@xyz.com', 'qazxsw')
+        # takes 3 parameters: table_name, key attr1, key attr2
+
+        c.write_batch('my_new_table')
+        # takes 1 parameter: table_name
+
+        c.update_item('my_new_table', 'abc@xyz.com', 'qazxsw', 'age', 21)
+        # takes 5 parameters: table_name, key attr1, key attr2, key of key value pair, value of key value pair
+
+    else:
+
+        c.put_data('my_new_table', 'abc@xyz.com', 'qazxsw', 'jay')
+        # takes 4 parameters: table_name, key attribute 1, key attribute 2 and some other attribute
+
+        c.write_batch(table_name)
+        # takes 1 parameter: table_name
+
+        c.get_data(table_name, 'abc@xyz.com', 'qazxsw')
+        # takes 3 parameters: table_name, key attr1, key attr2
+
+    c.delete_item(table_name, {'age': '21'})
+    # takes 2 parameters: table_name and key-value pair
+
+    c.delete_all_items(table_name)
+    # takes 1 parameter: table_name
+
+    c.delete_table(table_name)
+    # takes 1 parameter: table_name
