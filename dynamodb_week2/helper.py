bdd221d6cf9bcbb559e2ebb085431604bfa9fc5b
b-jayesh9 <jayshu.in@gmail.com>
2019-11-05 12:41:46 +0530

    updated helper file

--- dynamodb_week2/helper.py
+++ dynamodb_week2/helper.py
@@ -1,9 +1,17 @@
 import code as c
+import time
+
 table_name = 'my_new_table'
+
 if __name__ == '__main__':
     if table_name not in c.existing_tables:
+        print("creating")
         c.create_table('my_new_table', 'username', 'password')
         # takes 3 parameters: table_name, key attribute 1 and key attribute 2
+
+        c.dynamo_db.Table(table_name).wait_until_exists()
+
+        print("table created")
 
         c.put_data('my_new_table', 'abc@xyz.com', 'qazxsw', 'jay')
         # takes 4 parameters: table_name, key attribute 1, key attribute 2 and some other attribute
@@ -18,7 +26,7 @@ if __name__ == '__main__':
         # takes 5 parameters: table_name, key attr1, key attr2, key of key value pair, value of key value pair
 
     else:
-
+        print("putting")
         c.put_data('my_new_table', 'abc@xyz.com', 'qazxsw', 'jay')
         # takes 4 parameters: table_name, key attribute 1, key attribute 2 and some other attribute
 
@@ -28,11 +36,14 @@ if __name__ == '__main__':
         c.get_data(table_name, 'abc@xyz.com', 'qazxsw')
         # takes 3 parameters: table_name, key attr1, key attr2
 
-    c.delete_item(table_name, {'age': '21'})
+    '''
+    c.delete_item(table_name, {'username': 'abc@xyz.com', 'password': 'qazxsw'})
     # takes 2 parameters: table_name and key-value pair
-
+    
     c.delete_all_items(table_name)
     # takes 1 parameter: table_name
-
+    
+    time.sleep(5)
     c.delete_table(table_name)
     # takes 1 parameter: table_name
+    '''
